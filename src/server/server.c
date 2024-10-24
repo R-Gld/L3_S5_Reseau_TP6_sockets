4eca76aa9136298da687fd96e402945531f739b2
R-Gld <romain.galland@rgld.r>
2024-10-25 01:13:47 +0200

    Adding handle_error & handle_error_socket to beautify the code and improve readability.
    Adding comments
    Creating function close_socket to improve readability

--- src/server/server.c
+++ src/server/server.c
@@ -8,10 +8,16 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define LISTENING_ADDRESS "0.0.0.0"
+#define handle_error(vl, msg) \
+    if(vl == -1) { perror(msg); return EXIT_FAILURE; }
+#define handle_error_socket(vl, msg, server_sock_fd) \
+    if(vl == -1) { perror(msg); if (close(server_sock_fd) == -1) perror("close socket"); return EXIT_FAILURE; }
 
+#define LISTENING_ADDRESS "0.0.0.0" // The listening ip address (Example: 127.0.0.1 for lo / 0.0.0.0 for all)
 #define MESSAGE_SENT "Const message sent from the server. :)\n"  // Message sent to the client
 
+
+void close_sockets(int client_fd, int server_fd);
 /**
  * Params: argv[0] <port>
  */
@@ -38,19 +44,19 @@ int main(int argc, char **argv) {
     if(err == 0) { fprintf(stderr, "An error occurred while parsing the IP '%s'.\n", LISTENING_ADDRESS); return EXIT_FAILURE; }
 
     int server_sock_fd = socket(sa.sin_family, SOCK_STREAM, 0);
-    if(server_sock_fd == -1) { perror("socket"); return EXIT_FAILURE; }
+    handle_error(server_sock_fd, "socket");
 
     struct sockaddr_in peer_addr;
 
     err = bind(server_sock_fd, (struct sockaddr *) &sa, sizeof(struct sockaddr));
-    if(err == -1) { perror("bind"); if (close(server_sock_fd) == -1) perror("close socket"); return EXIT_FAILURE; }
+    handle_error_socket(err, "bind", server_sock_fd);
 
     err = listen(server_sock_fd, SOMAXCONN); // Here, `SOMAXCONN` as backlog to use `/proc/sys/net/core/somaxconn` (The system defined backlog value) By default on my system: 4096
-    if(err == -1) { perror("listen"); if (close(server_sock_fd) == -1) perror("close socket"); return EXIT_FAILURE; }
+    handle_error_socket(err, "listen", server_sock_fd);
 
     socklen_t peer_addr_size = sizeof(peer_addr);
     int client_socket_fd = accept(server_sock_fd, (struct sockaddr *) &peer_addr, &peer_addr_size);
-    if(client_socket_fd == -1) { perror("accept"); if(close(server_sock_fd) == -1) perror("close socket"); return EXIT_FAILURE; }
+    handle_error_socket(client_socket_fd, "accept", server_sock_fd);
 
     const char *message = MESSAGE_SENT;
 
@@ -61,22 +67,24 @@ int main(int argc, char **argv) {
     ssize_t byte_sent = send(client_socket_fd, message, strlen(message), 0);
     if(byte_sent == -1) {
         perror("send");
-        if (shutdown(client_socket_fd, SHUT_RDWR) == -1) perror("shutdown client socket ends");
-        if (close(client_socket_fd) == -1) perror("close client socket");
-
-        if(shutdown(server_sock_fd, SHUT_RDWR) == -1) perror("shutdown server socket ends");
-        if(close(server_sock_fd) == -1) perror("close server socket");
-
+        close_sockets(client_socket_fd, server_sock_fd);
         return EXIT_FAILURE;
     }
 
-    // Close every socket.
-    // Before using close(2), using shutdown(2) to shutdown gracefully the sockets.
-    if(shutdown(client_socket_fd, SHUT_RDWR) == -1) { perror("shutdown client socket ends"); return EXIT_FAILURE; }
-    if(close(client_socket_fd) == -1) { perror("close client socket"); return EXIT_FAILURE; }
-
-    if(shutdown(server_sock_fd, SHUT_RDWR) == -1) { perror("shutdown server socket ends"); return EXIT_FAILURE; }
-    if(close(server_sock_fd) == -1) { perror("close server socket"); return EXIT_FAILURE; }
-
+    close_sockets(client_socket_fd, server_sock_fd);
     return EXIT_SUCCESS;
+}
+
+/**
+ * Close every socket
+ * Before using close(2), using shutdown(2) to shutdown gracefully the sockets.
+ * @param client_fd the client socket file descriptor number
+ * @param server_fd the server socket file descriptor number
+ */
+void close_sockets(int client_fd, int server_fd) {
+    if (shutdown(client_fd, SHUT_RDWR) == -1) perror("shutdown client socket ends");
+    if (close(client_fd) == -1) perror("close client socket");
+
+    if(shutdown(server_fd, SHUT_RDWR) == -1) perror("shutdown server socket ends");
+    if(close(server_fd) == -1) perror("close server socket");
 }

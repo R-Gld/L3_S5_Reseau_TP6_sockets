e61dbe7713fff0d13eba1bf741f443f13e9816ab
R-Gld <romain.galland@rgld.r>
2024-11-06 11:57:52 +0100

    Update Makefile and client networking code
     - Modify CFLAGS in Makefile to include -std=c99 for C99 standard compliance.
     - Refactor client.c to use getaddrinfo for improved address resolution and compatibilit
     with both IPv4 and IPv6.
     - Remove hardcoded sockaddr_in structure and replace with addrinfo structure.
     - Add error handling for getaddrinfo and connection attempts.
     - Comment out old networking code for clarity.

--- src/client/client.c
+++ src/client/client.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <string.h>
 #include <unistd.h>
+#include <netdb.h>
 
 #define handle_error(vl, msg) \
     if(vl == -1) { perror(msg); return EXIT_FAILURE; }
@@ -32,7 +33,38 @@ int main(int argc, char **argv) {
         raw = 1;
     }
 
-    struct sockaddr_in sa;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0; hints.ai_protocol = 0;
+
+    struct addrinfo *result, *rp;
+
+    int s = getaddrinfo(address_str, argv[2], &hints, &result);
+    if(s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
+    }
+
+    int socket_fd;
+    for(rp = result; rp != NULL; rp = rp->ai_next) {
+        socket_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if(socket_fd == -1) continue;
+        if(connect(socket_fd, rp->ai_addr, rp->ai_addrlen) != -1)
+            break;
+
+        close(socket_fd);
+    }
+    freeaddrinfo(result);
+    if(rp == NULL) {
+        fprintf(stderr, "Could not connect\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    /*struct sockaddr_in sa;
     sa.sin_port = htons(port);
     sa.sin_family = AF_INET;
     bzero(sa.sin_zero, 8);
@@ -45,7 +77,7 @@ int main(int argc, char **argv) {
     handle_error(socket_fd, "socket");
 
     err = connect(socket_fd, (struct sockaddr*) &sa, sizeof(struct sockaddr));
-    handle_error_socket(err, "connect", socket_fd)
+    handle_error_socket(err, "connect", socket_fd)*/
 
     char buffer[BUFSIZ]; // Match the size from the BUFFER to the server.c
     ssize_t byte_read = 1;
